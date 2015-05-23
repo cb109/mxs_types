aeebf2e87d40b7344b3f4b6782f87cc973b9e3ff
Christoph Buelter <c.buelter@arcor.de>
2015-05-23 11:06:09 +0200

    Remove license header from scripts, instead place it in a separate file that is used as prefix for the build

--- misc/merge.py
+++ misc/merge.py
@@ -8,30 +8,30 @@ import os
 
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+LICENSE_FILE = os.path.join(ROOT_DIR, "LICENSE.txt")
 TARGET_FILE = os.path.join(ROOT_DIR, "mxs_types.ms")
 
 
-def remove_license_header(lines):
-    struct_found = False
-    for line in lines:
-        if line.strip().startswith("struct "):
-            struct_found = True
-        if struct_found:
-            yield line
+def get_license():
+    with open(LICENSE_FILE) as f:
+        return f.read()
 
 
 def main():
     scriptfiles = ["mxs_dict.ms",
                    "mxs_set.ms"]
 
+    separation = "\n" * 3
     with open(TARGET_FILE, "w") as f:
+        license = get_license()
+        f.write(license + separation)
         for scriptfile in scriptfiles:
             scriptpath = os.path.join(ROOT_DIR, scriptfile)
-            lines = open(scriptpath).readlines()
-            lines = list(remove_license_header(lines))
-            content = "".join(lines)
-            f.write(content + "\n" * 3)
+            content = open(scriptpath).read()
+            content = content.strip()
+            f.write(content + separation)
 
+    print ("Scripts merged to:", TARGET_FILE)
 
 if __name__ == "__main__":
     main()
