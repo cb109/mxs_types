70cf23f6469859f1e73b7b47b5906715ce7d45c5
Christoph Buelter <c.buelter@arcor.de>
2015-05-23 11:11:55 +0200

    Rename, move and adapt build script to root folder

--- build.py
+++ build.py
@@ -1,15 +1,19 @@
 """Merge the individual scripts into a single file for easy distribution.
 
 Call from the root directory like:
-    python misc/merge.py
+
+    python build.py
 
 """
 import os
 
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+SOURCE_DIR = os.path.join(ROOT_DIR, "mxs_types")
 LICENSE_FILE = os.path.join(ROOT_DIR, "LICENSE.txt")
 TARGET_FILE = os.path.join(ROOT_DIR, "mxs_types.ms")
+
+SEPARATION = "\n" * 3
 
 
 def get_license():
@@ -17,21 +21,22 @@ def get_license():
         return f.read()
 
 
-def main():
-    scriptfiles = ["mxs_dict.ms",
-                   "mxs_set.ms"]
+def build():
+    scriptfiles = [os.path.join(SOURCE_DIR, "mxs_dict.ms"),
+                   os.path.join(SOURCE_DIR, "mxs_set.ms")]
 
-    separation = "\n" * 3
+    os.remove(TARGET_FILE)
     with open(TARGET_FILE, "w") as f:
         license = get_license()
-        f.write(license + separation)
+        f.write(license + SEPARATION)
         for scriptfile in scriptfiles:
             scriptpath = os.path.join(ROOT_DIR, scriptfile)
             content = open(scriptpath).read()
             content = content.strip()
-            f.write(content + separation)
+            f.write(content + SEPARATION)
 
-    print ("Scripts merged to:", TARGET_FILE)
+    print ("Build created at:", TARGET_FILE)
+
 
 if __name__ == "__main__":
-    main()
+    build()
