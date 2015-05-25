e4465ab1e1f0e538a616996aa9e37feb72af949a
Christoph Buelter <c.buelter@arcor.de>
2015-05-25 21:38:11 +0200

    Add mxs_hashdict to build script

--- build.py
+++ build.py
@@ -13,6 +13,7 @@ LICENSE_FILE = os.path.join(ROOT_DIR, "LICENSE.txt")
 SOURCE_DIR = os.path.join(ROOT_DIR, "mxs_types")
 SOURCE_FILES =  [os.path.join(SOURCE_DIR, "mxs_dict.ms"),
                  os.path.join(SOURCE_DIR, "mxs_defaultdict.ms"),
+                 os.path.join(SOURCE_DIR, "mxs_hashdict.ms"),
                  os.path.join(SOURCE_DIR, "mxs_set.ms")]
 TARGET_FILE = os.path.join(ROOT_DIR, "mxs_types.ms")
 
